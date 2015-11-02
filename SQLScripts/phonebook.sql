ed5e5d5c5f40d3ab5f0f205b69e5166550eb2a52
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-02 22:53:42 +0200

    added changes into Scripts

--- SQLScripts/phonebook.sql
+++ SQLScripts/phonebook.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `phonebook`;
 USE `phonebook`;
 CREATE TABLE `contacts` (
 	`id` INT auto_increment NOT NULL,
-    `firstName` varchar(45),
-    `lastName` varchar(45),
+    `firstName` varchar(45) NOT NULL,
+    `lastName` varchar(45) NOT NULL,
     primary key(`id`)
 );
