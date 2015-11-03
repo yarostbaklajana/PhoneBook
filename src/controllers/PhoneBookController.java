d1e2d4497265e5c27e175f395656e64d7bfe72cf
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-03 13:05:23 +0200

    Added servlet-mapping for PhBookController; added mainpage.jsp; added changes into doGet method

--- src/controllers/PhoneBookController.java
+++ src/controllers/PhoneBookController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,30 +10,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.PhoneBookDAO;
+import exceptions.DAOException;
+import models.Contact;
+
 public class PhoneBookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default constructor.
-	 */
 	public PhoneBookController() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.print("Hello again)");
+		try {
+			PhoneBookDAO dao = new PhoneBookDAO();
+			ArrayList<Contact> contacts = dao.getAllContacts();
+			request.setAttribute("contacts", contacts);
+			request.setAttribute("size", contacts.size());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}			
+		
+		request.getRequestDispatcher("mainpage.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
