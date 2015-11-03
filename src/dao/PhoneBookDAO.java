eaa8308430a39eaad25740ff712aa5d9978eae36
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-03 11:18:41 +0200

    Added in PhoneBookDAO methods getAllContacts() , deleteContact(); Added empty constructor to Contact method;

--- src/dao/PhoneBookDAO.java
+++ src/dao/PhoneBookDAO.java
@@ -3,7 +3,11 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.Statement;
 
 import exceptions.DAOException;
 import models.Contact;
@@ -16,27 +20,58 @@ public class PhoneBookDAO {
 	public PhoneBookDAO() throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 	}
-	
+
 	public void addContact(Contact contact) throws DAOException {
 		final String insertStatement = "INSERT INTO `contacts` (`firstName`, `lastName`) VALUES (?, ?)";
-		try(Connection connection = connect();) {
+		try (Connection connection = connect();) {
 			System.out.println("Connected");
 			PreparedStatement statement = connection.prepareStatement(insertStatement);
 			statement.setString(1, contact.getFirstName());
 			statement.setString(2, contact.getLastName());
 			statement.execute();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			throw new DAOException("Can't connect to database");
+			throw new DAOException("Unable to save contact");
 		}
-		
-		
+
+	}
+
+	public ArrayList<Contact> getAllContacts() throws DAOException{
+		ArrayList<Contact> contacts = new ArrayList<>();
+		final String selectAllQuery = "SELECT * FROM `contacts`;";
+
+		try (Connection connection = connect();) {
+			Statement statement = (Statement) connection.createStatement();
+			ResultSet result = statement.executeQuery(selectAllQuery);
+
+			while (result.next()) {
+				Contact contact = new Contact();
+				contact.setFirstName(result.getString("`firstName`"));
+				contact.setLastName(result.getString("`lastName`"));
+				contacts.add(contact);
+			}
+
+			return contacts;
+
+		} catch (SQLException e) {			
+			throw new DAOException("Unable to load list of contacts");
+		}
 	}
 	
-
-	
-	public Connection connect() throws SQLException {
+	public void deleteContact(int id) throws DAOException {
+		final String deleteStatement = "DELETE FROM `contacts` WHERE `id`=" + id + ";";
 		
+		try(Connection connection = connect();) {
+			Statement statement = (Statement) connection.createStatement();
+			statement.executeUpdate(deleteStatement);
+			
+		} catch (SQLException e) {
+			throw new DAOException("Unable to delete contact");
+		}
+		
+	}
+
+	public Connection connect() throws SQLException {
+
 		return DriverManager.getConnection(hostName, userName, password);
 	}
 
--- src/models/Contact.java
+++ src/models/Contact.java
@@ -4,36 +4,40 @@ public class Contact {
 	private int id;
 	private String firstName;
 	private String lastName;
-	
+
 	public Contact(int id, String firstName, String lastName) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-	
-	public Contact(String firstName, String lastName) {		
+
+	public Contact(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-	
+
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
-	
 
 }
