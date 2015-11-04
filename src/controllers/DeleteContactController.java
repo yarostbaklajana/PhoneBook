0a73dc9379313697764a10051a4392d658e69081
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-04 14:51:42 +0200

    Added changes to post method

--- src/controllers/DeleteContactController.java
+++ src/controllers/DeleteContactController.java
@@ -13,30 +13,32 @@ import exceptions.DAOException;
 @WebServlet("/DeleteContactController")
 public class DeleteContactController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
-		
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Integer id = Integer.valueOf(request.getParameter("id"));
-		 
 		PhoneBookDAO dao;
-		try {
-			dao = new PhoneBookDAO();
-			dao.deleteContact(id);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (id != 0) {
+			
+			try {
+				dao = new PhoneBookDAO();
+				dao.deleteContact(id);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DAOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		
-		request.getRequestDispatcher("mainpage.jsp").forward(request, response);
+		response.sendRedirect("/PhoneBook/phonebook");
+
 	}
 
 }
