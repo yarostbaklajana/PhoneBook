3d2153d515d209f7a850e4179f55e726a480307c
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-05 10:22:04 +0200

    changed catch scope

--- src/controllers/AddContactController.java
+++ src/controllers/AddContactController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,59 +20,54 @@ import models.Contact;
 @WebServlet("/AddContactController")
 public class AddContactController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setAttribute("firstName", "");
 		request.setAttribute("lastName", "");
 		request.getRequestDispatcher("addpage.jsp").forward(request, response);
-		
-		
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
-		
-		
-		
+
 		PhoneBookDAO dao;
-		
+
 		List<String> errorMessages = new ArrayList<>();
-		
-		if(firstName.equals("") || firstName == null) {
+
+		if (firstName.equals("") || firstName == null) {
 			errorMessages.add("The first name field is empty!");
 			request.setAttribute("firstName", "");
 			request.setAttribute("lastName", lastName);
 		}
-		
-		if(lastName.equals("") || lastName == null) {
+
+		if (lastName.equals("") || lastName == null) {
 			errorMessages.add("The last name field is empty!");
 			request.setAttribute("firstName", firstName);
 			request.setAttribute("lastName", "");
 		}
-		
-		if(!errorMessages.isEmpty()) {
+
+		if (!errorMessages.isEmpty()) {
 			request.setAttribute("errorMessages", errorMessages);
 			request.getRequestDispatcher("addpage.jsp").forward(request, response);
 			return;
 		}
-		
-		
-		
+
 		try {
 			dao = new PhoneBookDAO();
 			dao.addContact(new Contact(firstName, lastName));
 		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.getMessage();;
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		response.sendRedirect("/PhoneBook/phonebook");
-		
-		
+
 	}
 
 }
