fde1f1cb09cde533d887a30f6104a2e35e54d3b9
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-05 10:08:21 +0200

    changed doPost and doGet methods

--- src/controllers/AddContactController.java
+++ src/controllers/AddContactController.java
@@ -3,7 +3,7 @@ package controllers;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.ErrorManager;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,27 +23,39 @@ public class AddContactController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("firstName", "");
+		request.setAttribute("lastName", "");
 		request.getRequestDispatcher("addpage.jsp").forward(request, response);
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
+		
+		
+		
 		PhoneBookDAO dao;
 		
 		List<String> errorMessages = new ArrayList<>();
 		
-		if(firstName == null) {
-			errorMessages.add("The firstName field is empty!");
+		if(firstName.equals("") || firstName == null) {
+			errorMessages.add("The first name field is empty!");
+			request.setAttribute("firstName", "");
+			request.setAttribute("lastName", lastName);
 		}
 		
-		if(lastName == null) {
-			errorMessages.add("The lastName field is empty!");
+		if(lastName.equals("") || lastName == null) {
+			errorMessages.add("The last name field is empty!");
+			request.setAttribute("firstName", firstName);
+			request.setAttribute("lastName", "");
 		}
 		
 		if(!errorMessages.isEmpty()) {
 			request.setAttribute("errorMessages", errorMessages);
 			request.getRequestDispatcher("addpage.jsp").forward(request, response);
+			return;
 		}
 		
 		
