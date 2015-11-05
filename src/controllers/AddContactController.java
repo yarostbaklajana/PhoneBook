301a5b6a67e8433302bac48f50fe4d0771193722
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-05 09:21:49 +0200

    changed doPost method

--- src/controllers/AddContactController.java
+++ src/controllers/AddContactController.java
@@ -1,11 +1,19 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.ErrorManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.PhoneBookDAO;
+import exceptions.DAOException;
+import models.Contact;
 
 /**
  * Servlet implementation class AddContactController
@@ -15,12 +23,45 @@ public class AddContactController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("addpage.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		PhoneBookDAO dao;
 		
-		doGet(request, response);
+		List<String> errorMessages = new ArrayList<>();
+		
+		if(firstName == null) {
+			errorMessages.add("The firstName field is empty!");
+		}
+		
+		if(lastName == null) {
+			errorMessages.add("The lastName field is empty!");
+		}
+		
+		if(!errorMessages.isEmpty()) {
+			request.setAttribute("errorMessages", errorMessages);
+			request.getRequestDispatcher("addpage.jsp").forward(request, response);
+		}
+		
+		
+		
+		try {
+			dao = new PhoneBookDAO();
+			dao.addContact(new Contact(firstName, lastName));
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("/PhoneBook/phonebook");
+		
+		
 	}
 
 }
